d277c395731fee511c68484e8968ac2fd6c556b3
EoinMcD <EoinMcD@users.noreply.github.com>
2020-10-14 19:01:24 +0100

    Test Push

--- app/src/main/java/com/example/socialmedia/Post.java
+++ app/src/main/java/com/example/socialmedia/Post.java
@@ -8,19 +8,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.errorprone.annotations.Var;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Post extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    public static final String Message = "ie.ul.myfirstapp.EXTRA_MESSAGE";
 
 
     @Override
@@ -32,13 +43,34 @@ public class Post extends AppCompatActivity {
     public void post(View view){
         CollectionReference Posts = db.collection("Posts");
 
-        db.collection("Posts");
-         EditText editText = (EditText) findViewById(R.id.Message);
+
+
+
+        EditText editText = (EditText) findViewById(R.id.Message);
         String PostMessage = editText.getText().toString();
         Map<String,Object> Posts1 = new HashMap<>();
-        Posts1.put("Post",PostMessage);
+        //Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String ts=Long.toString(System.currentTimeMillis());
 
-        Posts.document(PostMessage).set(Posts1);
+        String name = "";
+        for(int i =0; i<13;i++) {
+            name+=(9- (Character.getNumericValue(ts.charAt(i))));
+
+        }
+       // Date date=new Date(ts.getTime());
+        //DateFormat df = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        //String Time = df.format(ts);//I know this can be broken by changing device time but servertimestamp doesnt work and I don't really have a different solution rn
+       // System.out.println(timestamp);
+
+
+
+
+
+
+
+        Posts1.put(name,PostMessage);
+
+        Posts.document(name).set(Posts1);
 
         Intent intent = new Intent(this,Reading.class);
 
